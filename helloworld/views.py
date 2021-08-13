6cd139a9b00e9586c9496f978aae6c0c35d2e0bb
Lee Jaeseong <fg9658@gmail.com>
2021-08-13 18:09:01 +0900

    proj init

---
+++ helloworld/views.py
@@ -0,0 +1,10 @@
+
+from django.http import JsonResponse
+
+from rest_framework.views import APIView
+
+
+class HelloWorld(APIView):
+
+    def get(self, request):
+        return JsonResponse({"hello": "world"}, status=200)
